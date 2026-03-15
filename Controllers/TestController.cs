73f59479d34c0b9f475ac87246e9db5ea7b68c70
chirag03169 <chiragrane.03@gmail.com>
2026-03-15 10:25:15 +0530

    Added Hello API

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApplication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("hello")]
+        public string GetHello()
+        {
+            return "Hello from Web API";
+        }
+    }
+}
